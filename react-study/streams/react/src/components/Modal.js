3f3fc1c4445e931db52fb1f4af155a116fbf3195
ksh-code <seonghoon.kim@cre.ma>
2019-04-14 20:21:58 +0900

    FEATURE - Dismiss Bubble-Up

--- react-study/streams/react/src/components/Modal.js
+++ react-study/streams/react/src/components/Modal.js
@@ -5,7 +5,7 @@ import history from '../history';
 const Modal = props => {
   return ReactDOM.createPortal(
     <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
+      <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
         <div className="header">Delete Stream</div>
         <div className="content">Are you sure you want to delete Stream?</div>
         <div className="actions">

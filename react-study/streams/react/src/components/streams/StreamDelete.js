0da1d8ce0edae3bbc481cfa21a07f9f1990699df
ksh-code <seonghoon.kim@cre.ma>
2019-04-14 20:32:43 +0900

    REFACTOR - div 를 React.Fragment 로 변경
    ### Desc
    - Fragment 는 rendering 되지 않는다.

--- react-study/streams/react/src/components/streams/StreamDelete.js
+++ react-study/streams/react/src/components/streams/StreamDelete.js
@@ -3,10 +3,10 @@ import Modal from '../Modal'
 
 const StreamDelete = () => {
   const actions = (
-    <div className="actions">
+    <>
       <button className="ui negative button">Delete</button>
       <button className="ui button">Cancel</button>
-    </div>
+    </>
   );
 
   return (
